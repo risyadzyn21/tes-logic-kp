655d9a8663508c42a7269ee4ea1463da6a9726dc
risyad <risyadzyn@gmail.com>
2021-12-23 13:27:02 +0700

    number 2

--- tes-logic.js
+++ tes-logic.js
@@ -1,3 +1,4 @@
+// 1.
 function findZAndThree() {
   for (let i = 1; i <= 30; i++) {
     if (i % 3 == 0 && i % 7 === 0) {
@@ -10,4 +11,22 @@ function findZAndThree() {
   }
 }
 
-findZAndThree()
+// findZAndThree()
+
+// 2.
+let story = 'Berikut adalah kisah sang gajah. Sang gajah memiliki teman serigala bernama DoeSang. Gajah sering dibela oleh serigala ketika harimau mendekati gajah.'
+
+function findAnimal() {
+  const search = story.split(' ')
+  for (let i = 0; i < search.length; i++) {
+    if (search[i] = 'sang gajah') {
+      console.log('sang gajah')
+    } else if (search[i] = 'serigala') {
+      console.log('serigala')
+    } else if (search[i] = 'harimau') {
+      console.log('harimau')
+    }
+  }
+}
+
+findAnimal()
